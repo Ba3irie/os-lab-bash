f97d23dff700d4cc0d5fa4c4a02d4bdaa30a4536
Ba3irie <129565470+Ba3irie@users.noreply.github.com>
2025-12-25 14:11:21 +0000

    Update scripts

--- q2_for_loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Print numbers from 1 to 5 using a for loop
-
-for i in {1..5}; do
-  echo "iteration $i"
-done
--- script2.sh
+++ script2.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
+# Print numbers from 1 to 5 using a while loop
+count=1
+while [ $count -le 5 ]; do
+echo "Count is $count"
+((count++))
+done
+echo "Finished counting!"
 # This is script2.sh
