8d28ec8f34c0697d1ca80d8c9f0828bca4ff88bb
Ba3irie <129565470+Ba3irie@users.noreply.github.com>
2025-12-25 14:14:20 +0000

    Update scripts

--- q4_until_loop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Print numbers from 1 to 5 using an until loop
-count=1
-until [ $count -gt 5 ]; do
-echo "Count is $count"
-((count++))
-done
--- script3.sh
+++ script3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+# Print numbers from 1 to 5 using an until loop
+count=1
+until [ $count -gt 5 ]; do
+echo "Count is $count"
+((count++))
+done
 # This is script3.sh
